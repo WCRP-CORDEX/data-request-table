59c885454a824c4d90bd21d8cb4d747340c83392
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2024-07-24 11:45:26 +0200

    added test

--- ci/tests.py
+++ ci/tests.py
@@ -6,8 +6,20 @@ from .utils import cmor_tables, tables
 
 
 @pytest.mark.parametrize("dreq_file", tables)
+def test_no_duplicates(dreq_file):
+    """assert all entries of a data request are unique"""
+    dreq_df = pd.read_csv(dreq_file)
+    assert dreq_df.loc[dreq_df.duplicated()].empty
+    assert (
+        dreq_df.drop(columns="priority")
+        .loc[dreq_df.drop(columns="priority").duplicated()]
+        .empty
+    )
+
+
+@pytest.mark.parametrize("dreq_file", tables)
 def test_all_in_cmor_tables(dreq_file):
-    """test if all entries of a data request are registered in the cmor tables"""
+    """assert all entries of a data request are registered in the cmor tables"""
     cmor_df = pd.read_csv(cmor_tables)
     dreq_df = pd.read_csv(dreq_file).drop(columns="priority")
     # dreq_df.loc[0, "out_name"] = "xxx"
